f34647b9d481b03d407f54d4b22873cfdd2993bd
Roshan kumar singh <roshanpatel.6702@gmail.com>
2025-02-11 01:56:07 +0530

    conversion of datatypes

---
+++ 01_BASICS/03_ConversionOperation.js
@@ -0,0 +1,48 @@
+/*let score="33"  //string
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumbar= Number(score)  //changed string to numbar by Number Method, we can  change number to string by String() 
+console.log(typeof valueInNumbar);
+console.log(valueInNumbar);
+
+score="33abc"
+console.log(typeof score);
+let valueInNumber= Number(score)  // cant change it if char and number both are mixed, lekin fir bhi wo number  output dega
+console.log( typeof valueInNumber)
+console.log(valueInNumber) //output:NaN
+*/
+// score=null
+ //object
+//change it to 0
+ //number
+  //output: 0
+
+//   score=undefined
+//undefined
+//number
+//NaN
+
+/*
+console.log(typeof score); 
+let valueInNumbere= Number(score) 
+console.log( typeof valueInNumbere)
+console.log(valueInNumber)
+*/
+
+//for boolean   true=>1;false=>0;
+//IF STRING IS NOT CONVERGABLE GIVES NaN(Not a number)
+
+
+let  isLoggedin=1;
+let booleanIsLoogedIn=Boolean(isLoggedin);
+//console.log(booleanIsLoogedIn);
+
+// 1=>true; 0=> false
+//""=> false; "hitesh"=>true
+
+let someNumber=33
+let numToString=String(someNumber);
+console.log(numToString);
+console.log(typeof numToString); //yes converted into string
