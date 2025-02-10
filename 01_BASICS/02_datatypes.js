2e917304bc37005a2d9cf55a85fff38aadf1509b
Roshan kumar singh <roshanpatel.6702@gmail.com>
2025-02-11 00:20:43 +0530

    datatypes

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all JS code as newer version
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log(`Hitesh`)
+
+let name="Roshan" //string 
+let age=18 //num
+let isLoggedin=false //boolean
+
+//PRIMITIVE DATA TYPES
+
+// numbe => 2 to the power 53
+//bigInt
+//string=> "" or ``
+//boolean=>true/false
+//null =>standalon value (representation of empty value)
+//undefined=> value not assign yet
+//symbol=>unique
+
+
+
+//OBJECT
+
+console.log(typeof(`hitesh`))
+console.log(typeof(6))
+console.log(typeof(null)) //object
