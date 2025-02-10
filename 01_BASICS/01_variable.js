4440a3fafaffa797fdd11af2d28126fb544da5a9
Roshan kumar singh <roshanpatel.6702@gmail.com>
2025-02-11 00:18:24 +0530

    variable

---
+++ 01_BASICS/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=12345
+let accountEmail="Roshan@google.com"
+var accountPass="23456"
+ accountcity="jaipur"
+// accountId=2  //not allowed
+accountEmail=`Bipin@google.com` //allowed
+accountPass="89076"  //allowed
+ accountcity=`varansi`  //allowed
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+console.table([accountId,accountEmail,accountPass,accountcity])
