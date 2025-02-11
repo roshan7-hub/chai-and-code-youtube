96afeac477e81bc8586797693f0160522cdc2970
Roshan kumar singh <roshanpatel.6702@gmail.com>
2025-02-11 19:04:43 +0530

    conversion & operation

--- 01_BASICS/03_ConversionOperation.js
+++ 01_BASICS/03_ConversionOperation.js
@@ -35,14 +35,73 @@ console.log(valueInNumber)
 //IF STRING IS NOT CONVERGABLE GIVES NaN(Not a number)
 
 
+/*
 let  isLoggedin=1;
 let booleanIsLoogedIn=Boolean(isLoggedin);
-//console.log(booleanIsLoogedIn);
+console.log(booleanIsLoogedIn);
+*/
 
 // 1=>true; 0=> false
 //""=> false; "hitesh"=>true
 
+/*
 let someNumber=33
 let numToString=String(someNumber);
 console.log(numToString);
-console.log(typeof numToString); //yes converted into string
+console.log(typeof numToString); //yes converted into string 
+*/
+
+// *************OPERATION**************//
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2)  //4
+// console.log(2-2)  //0
+// console.log(2*2)  //4
+// console.log(2**3)  //8
+// console.log(2/2)  //1
+// console.log(2%2)  //0
+
+/*
+//add strings
+let str1=`Roshan`
+let str2=` kumar`
+let str3=` singh`
+
+let str4=str1+str2+str3;
+console.log(str4);
+*/
+
+/*
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122  //agar first me string hai to sabhi ko string ki tarah treat kara jayega
+console.log(1+2+"2");  //32   //isame suru me number hai to numbar ki tarh treat kara jayega except last one kyoki wo string ki tarh define hai
+*/
+
+
+// console.log(3+4*5%3);  //5
+// console.log((3+4)*5%3);   //2
+
+// console.log(+true) //1
+// console.log(true)   //true
+// console.log(+"") //0
+
+/*
+let num1,mun2,num3
+
+num1=num2=num3=2+2   //all become 4
+console.log(num1)  //4
+console.log(num2)  //4
+console.log(num3)  //4
+*/
+
+/*
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)  //101
+gameCounter++
+console.log(gameCounter)  //102
+*/
